bc0206544a7e84ae24f45a133b605cffe60b529c
stephann <3025661+stephannv@users.noreply.github.com>
2024-11-18 17:17:48 -0300

    Make generators compatible with importmaps

--- lib/generators/ruby_ui/component_generator.rb
+++ lib/generators/ruby_ui/component_generator.rb
@@ -43,8 +43,11 @@ module RubyUI
           copy_file file_path, Rails.root.join("app/javascript/controllers/ruby_ui", controller_file_name)
         end
 
-        say "Updating Stimulus controllers manifest"
-        run "rake stimulus:manifest:update"
+        # Importmap doesn't have controller manifest, instead it uses `eagerLoadControllersFrom("controllers", application)`
+        if !using_importmap?
+          say "Updating Stimulus controllers manifest"
+          run "rake stimulus:manifest:update"
+        end
       end
 
       def install_dependencies
@@ -90,13 +93,47 @@ module RubyUI
       end
 
       def install_js_package(package)
-        if File.exist?(Rails.root.join("package-lock.json"))
-          run "npm install #{package}"
-        elsif File.exist?(Rails.root.join("yarn.lock"))
+        if using_importmap?
+          pin_with_importmap(package)
+        elsif using_yarn?
           run "yarn add #{package}"
+        elsif using_npm?
+          run "npm install #{package}"
         else
           say "Could not detect the package manager, you need to install '#{package}' manually", :red
         end
+      end
+
+      def pin_with_importmap(package)
+        case package
+        when "motion"
+          pin_motion
+        when "tippy.js"
+          pin_tippy_js
+        else
+          run "bin/importmap pin #{package}"
+        end
+      end
+
+      def pin_motion
+        say <<~TEXT
+          WARNING: Installing motion from CDN because `bin/importmap pin motion` doesn't download the correct file.
+        TEXT
+
+        inject_into_file Rails.root.join("config/importmap.rb"), <<~RUBY
+          pin "motion", to: "https://cdn.jsdelivr.net/npm/motion@11.11.17/+esm"\n
+        RUBY
+      end
+
+      def pin_tippy_js
+        say <<~TEXT
+          WARNING: Installing tippy.js from CDN because `bin/importmap pin tippy.js` doesn't download the correct file.
+        TEXT
+
+        inject_into_file Rails.root.join("config/importmap.rb"), <<~RUBY
+          pin "tippy.js", to: "https://cdn.jsdelivr.net/npm/tippy.js@6.3.7/+esm"
+          pin "@popperjs/core", to: "https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/+esm"\n
+        RUBY
       end
 
       def dependencies
@@ -104,6 +141,14 @@ module RubyUI
 
         @dependencies[component_folder_name]
       end
+
+      def using_importmap?
+        File.exist?(Rails.root.join("config/importmap.rb")) && File.exist?(Rails.root.join("bin/importmap"))
+      end
+
+      def using_npm? = File.exist?(Rails.root.join("package-lock.json"))
+
+      def using_yarn? = File.exist?(Rails.root.join("yarn.lock"))
     end
   end
 end
