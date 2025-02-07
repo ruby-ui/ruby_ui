9be9965e92824a2d6f88cc0d1919de0ad737d023
Karine Vieira <kv.vieira1@gmail.com>
2025-02-07 13:38:06 -0300

    applies code review suggestions

--- lib/ruby_ui/combobox/combobox_toggle_all_checkbox.rb
+++ lib/ruby_ui/combobox/combobox_toggle_all_checkbox.rb
@@ -3,16 +3,21 @@
 module RubyUI
   class ComboboxToggleAllCheckbox < Base
     def view_template
-      render RubyUI::ComboboxCheckbox.new(**attrs)
+      input(type: "checkbox", **attrs)
     end
 
     private
 
     def default_attrs
       {
+        class: [
+          "peer h-4 w-4 shrink-0 rounded-sm border border-primary ring-offset-background accent-primary",
+          "focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2",
+          "disabled:cursor-not-allowed disabled:opacity-50"
+        ],
         data: {
-          ruby_ui__combobox_target: "toggle",
-          action: "change->ruby-ui--combobox#toggleInputs"
+          ruby_ui__combobox_target: "toggleAll",
+          action: "change->ruby-ui--combobox#toggleAllItems"
         }
       }
     end
