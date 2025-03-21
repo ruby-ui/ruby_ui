18e4cff1ac3c89649920dc7e39fa0e1ae7ffb391
Lucas Sousa <ls442336@gmail.com>
2025-03-20 21:54:57 -0300

    add sidebar submenu item

--- lib/ruby_ui/sidebar/mobile_sidebar.rb
+++ lib/ruby_ui/sidebar/mobile_sidebar.rb
@@ -4,15 +4,13 @@ module RubyUI
   class MobileSidebar < Base
     def view_template(&)
       div(**attrs) do
-        "teste"
       end
     end
 
     private
 
     def default_attrs
-      {
-      }
+      {}
     end
   end
 end
