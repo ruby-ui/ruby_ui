ce5c958c781b85ce92caac4d5363f7d4cb8df118
Lucas Sousa <ls442336@gmail.com>
2025-03-22 14:46:11 -0300

    fix icon attributes

--- lib/ruby_ui/sidebar/sidebar_trigger.rb
+++ lib/ruby_ui/sidebar/sidebar_trigger.rb
@@ -13,7 +13,7 @@ module RubyUI
 
     def default_attrs
       {
-        class: "h-7 w-7",
+        class: "h-7 w-7 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0",
         data: {
           sidebar: "trigger"
         }
@@ -23,17 +23,17 @@ module RubyUI
     def panel_left_icon
       svg(
         xmlns: "http://www.w3.org/2000/svg", 
-        width: 24, 
-        height: 24, 
-        viewBox: "0 0 24 24", 
-        fill: "none", 
-        stroke: "currentColor", 
-        strokeWidth: 2, 
-        strokeLinecap: "round", 
-        strokeLinejoin: "round", 
+        width: "24",
+        height: "24",
+        viewBox: "0 0 24 24",
+        fill: "none",
+        stroke: "currentColor",
+        stroke_width: "2",
+        stroke_linecap: "round",
+        stroke_linejoin: "round",
         class: "lucide lucide-panel-left"
       ) do |s|
-        s.rect(width: 18, height: 18, x: 3, y: 3, rx: 2)
+        s.rect(width: "18", height: "18", x: "3", y: "3", rx: "2")
         s.path(d: "M9 3v18")
       end
     end
