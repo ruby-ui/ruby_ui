26995d324ae805c212f88e0eaf48f76ee1ff8358
Karine Vieira <kv.vieira1@gmail.com>
2025-03-22 17:00:24 -0300

    Replace tailwindcss-animate with tw-animate-css to support Tailwind 4

--- lib/generators/ruby_ui/install/install_generator.rb
+++ lib/generators/ruby_ui/install/install_generator.rb
@@ -62,7 +62,7 @@ module RubyUI
         end
       end
 
-      def install_tailwind_animate
+      def install_tw_animate_css
         say "Installing tw-animate-css plugin"
 
         install_js_package("tw-animate-css")
