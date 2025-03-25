920617fba25442f3ae4f6971c30dbc4ac46e6088
stephann <3025661+stephannv@users.noreply.github.com>
2025-03-25 10:44:33 -0300

    Prepare generator for jsbundling + Tailwind V4 (#237)
    Co-authored-by: Seth Horsley <isethi@me.com>

--- lib/generators/ruby_ui/install/install_generator.rb
+++ lib/generators/ruby_ui/install/install_generator.rb
@@ -14,17 +14,12 @@ module RubyUI
         say "Checking for phlex-rails"
 
         if gem_installed?("phlex-rails")
-          if Gem::Specification.find_by_name("phlex-rails").version < "2.0.0.beta2"
-            say "You need to upgrade to phlex-rails 2 to use RubyUI", :red
-            exit
-          else
-            say "phlex-rails is already installed", :green
-          end
+          say "phlex-rails is already installed", :green
         else
           say "Adding phlex-rails to Gemfile"
-          run %(bundle add phlex-rails --github="phlex-ruby/phlex-rails")
+          run %(bundle add phlex-rails)
 
-          say "Running phlex-rails structure"
+          say "Generating phlex-rails structure"
           run "bin/rails generate phlex:install"
         end
       end
@@ -52,31 +47,26 @@ module RubyUI
         end
       end
 
-      def add_tailwind_config
-        say "Adding RubyUI config to tailwind config"
+      def add_tailwind_css
+        say "Adding Tailwind css"
 
-        if using_tailwindcss_rails_gem?
-          template "application.css.erb", Rails.root.join("app/assets/tailwind/application.css")
+        css_path = if using_importmap?
+          Rails.root.join("app/assets/tailwind/application.css")
         else
-          template "application.tailwind.css.erb", Rails.root.join("app/assets/stylesheets/application.tailwind.css")
+          Rails.root.join("app/assets/stylesheets/application.tailwind.css")
         end
+
+        template "tailwind.css.erb", css_path
       end
 
-      def install_tw_animate_css
+      def install_tailwind_plugins
         say "Installing tw-animate-css plugin"
-
         install_js_package("tw-animate-css")
-      end
 
-      def install_tailwindcss_forms
         say "Installing @tailwindcss/forms plugin"
-
         install_js_package("@tailwindcss/forms")
-      end
 
-      def install_tailwindcss_typography
         say "Installing @tailwindcss/typography plugin"
-
         install_js_package("@tailwindcss/typography")
       end
 
