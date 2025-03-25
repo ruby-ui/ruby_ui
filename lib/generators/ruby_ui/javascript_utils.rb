673fcbcf38e17aa4774b47c167dbb54e1bfd93a2
stephann <3025661+stephannv@users.noreply.github.com>
2025-03-25 10:33:05 -0300

    Prepare generator for jsbundling + Tailwind V4

--- lib/generators/ruby_ui/javascript_utils.rb
+++ lib/generators/ruby_ui/javascript_utils.rb
@@ -31,6 +31,27 @@ module RubyUI
       def using_npm? = File.exist?(Rails.root.join("package-lock.json"))
 
       def using_yarn? = File.exist?(Rails.root.join("yarn.lock"))
+
+      def pin_motion
+        say <<~TEXT
+          WARNING: Installing motion from CDN because `bin/importmap pin motion` doesn't download the correct file.
+        TEXT
+
+        inject_into_file Rails.root.join("config/importmap.rb"), <<~RUBY
+          pin "motion", to: "https://cdn.jsdelivr.net/npm/motion@11.11.17/+esm"\n
+        RUBY
+      end
+
+      def pin_tippy_js
+        say <<~TEXT
+          WARNING: Installing tippy.js from CDN because `bin/importmap pin tippy.js` doesn't download the correct file.
+        TEXT
+
+        inject_into_file Rails.root.join("config/importmap.rb"), <<~RUBY
+          pin "tippy.js", to: "https://cdn.jsdelivr.net/npm/tippy.js@6.3.7/+esm"
+          pin "@popperjs/core", to: "https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/+esm"\n
+        RUBY
+      end
     end
   end
 end
