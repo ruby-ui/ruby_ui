9b4e59b23fde7c50f996e732763cc9ab875b408c
Manuel Costa Reis <manuel@creis.xyz>
2025-04-03 09:10:44 +0100

    Remove ring-ring classes
    This classes do not appear to be part of tailwind and as such may not be doing anything in these components.

--- lib/ruby_ui/tabs/tabs_content.rb
+++ lib/ruby_ui/tabs/tabs_content.rb
@@ -19,7 +19,7 @@ module RubyUI
           ruby_ui__tabs_target: :content,
           value: @value
         },
-        class: "mt-2 ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 hidden"
+        class: "mt-2 ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 hidden"
       }
     end
   end
