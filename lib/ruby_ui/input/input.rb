71994fe2708794a5b66de9ebc33138fe941de3c3
Jean Pierry <pierrybm@gmail.com>
2025-05-07 12:11:39 -0300

    Update input.rb
    Signed-off-by: Jean Pierry <pierrybm@gmail.com>

--- lib/ruby_ui/input/input.rb
+++ lib/ruby_ui/input/input.rb
@@ -23,8 +23,8 @@ module RubyUI
           "flex h-9 w-full rounded-md border bg-background px-3 py-1 text-sm shadow-sm transition-colors border-border",
           "placeholder:text-muted-foreground",
           "disabled:cursor-not-allowed disabled:opacity-50",
-          "focus-visible:outline-none focus-visible:ring-ring",
           "file:border-0 file:bg-transparent file:text-sm file:font-medium",
+          "focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring",
           "aria-disabled:cursor-not-allowed aria-disabled:opacity-50 aria-disabled:pointer-events-none"
         ]
       }
