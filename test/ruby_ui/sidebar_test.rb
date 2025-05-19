95dc9a28ba5c4d460fa2845cba308d518bb87431
Lucas Sousa <30642647+lsouoliveira@users.noreply.github.com>
2025-05-19 16:08:39 -0300

    Fix sidebar typo (#294)
    * Fix sidebar typo
    
    * Add a test case for the non collapsible variant

--- test/ruby_ui/sidebar_test.rb
+++ test/ruby_ui/sidebar_test.rb
@@ -63,6 +63,66 @@ class RubyUI::SidebarTest < ComponentTest
     assert_match(/Footer/, output)
   end
 
+  def test_render_non_collapsible_sidebar
+    output = phlex do
+      RubyUI.SidebarWrapper do
+        RubyUI.Sidebar(collapsible: :none) do
+          RubyUI.SidebarHeader do
+            RubyUI.SidebarGroup do
+              RubyUI.SidebarGroupContent do
+                RubyUI.SidebarInput(id: "search", placeholder: "Search the docs")
+              end
+            end
+          end
+          RubyUI.SidebarContent do
+            RubyUI.SidebarGroup do
+              RubyUI.SidebarGroupLabel { "Application" }
+              RubyUI.SidebarGroupAction { "Group Action" }
+              RubyUI.SidebarGroupContent do
+                RubyUI.SidebarMenu do
+                  RubyUI.SidebarMenuItem do
+                    RubyUI.SidebarMenuSub do
+                      RubyUI.SidebarMenuSubItem do
+                        RubyUI.SidebarMenuSubButton(as: :a, href: "#") { "Sub Item 1" }
+                      end
+                    end
+                  end
+                  RubyUI.SidebarMenuItem do
+                    RubyUI.SidebarMenuButton(as: :a, href: "#") { "Settings" }
+                    RubyUI.SidebarMenuAction { "Settings" }
+                  end
+                  RubyUI.SidebarMenuItem do
+                    RubyUI.SidebarMenuButton { "Dashboard" }
+                    RubyUI.SidebarMenuAction { "Dashboard" }
+                    RubyUI.SidebarMenuBadge { "Dashboard Badge" }
+                  end
+                  RubyUI.SidebarMenuItem do
+                    RubyUI.SidebarMenuSkeleton()
+                  end
+                end
+              end
+            end
+            RubyUI.SidebarSeparator()
+          end
+          RubyUI.SidebarFooter { "Footer" }
+          RubyUI.SidebarRail()
+        end
+        RubyUI.SidebarInset do
+          RubyUI.SidebarTrigger()
+        end
+      end
+    end
+
+    assert_match(/Search the docs/, output)
+    assert_match(/Application/, output)
+    assert_match(/Group Action/, output)
+    assert_match(/Sub Item 1/, output)
+    assert_match(/Settings/, output)
+    assert_match(/Dashboard/, output)
+    assert_match(/Dashboard Badge/, output)
+    assert_match(/Footer/, output)
+  end
+
   def test_with_side_right
     output = phlex do
       RubyUI.Sidebar(side: :right)
