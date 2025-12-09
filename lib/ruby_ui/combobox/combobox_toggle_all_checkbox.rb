7a2abb79033ff047e53952cf136aaaf04d9f1809
Jean Mendonça <pierrybm@gmail.com>
2025-12-09 18:24:34 -0300

    standard

--- lib/ruby_ui/combobox/combobox_toggle_all_checkbox.rb
+++ lib/ruby_ui/combobox/combobox_toggle_all_checkbox.rb
@@ -15,7 +15,7 @@ module RubyUI
           "disabled:cursor-not-allowed disabled:opacity-50",
           "checked:bg-primary checked:text-primary-foreground",
           "aria-disabled:cursor-not-allowed aria-disabled:opacity-50 aria-disabled:pointer-events-none",
-          "focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2",
+          "focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2"
         ],
         data: {
           ruby_ui__combobox_target: "toggleAll",
