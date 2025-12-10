b329b5fb884495ab7ccbb46505079c6a32928675
Djalma Araújo <djalma@nossomos.cc>
2025-12-10 18:13:49 -0300

    Add missing stub classes and methods for documentation
    - Update VisualCodeExample to accept description and src parameters
    - Add InlineLink, Text, Alert, AlertTitle, AlertDescription helpers
    - Add route helper stubs (docs_*_path methods)
    - Add sidebar example constants

--- lib/ruby_ui/docs/base.rb
+++ lib/ruby_ui/docs/base.rb
@@ -10,5 +10,61 @@ module Views
     def component_files(component_name)
       []
     end
+
+    # Text helper for wrapping paragraphs
+    def Text(&)
+      p(&)
+    end
+
+    # InlineLink helper for documentation links
+    def InlineLink(href:, target: nil, class: nil, &)
+      a(href: href, target: target, class: binding.local_variable_get(:class), &)
+    end
+
+    # Alert component helpers
+    def Alert(&)
+      div(&)
+    end
+
+    def AlertTitle(&)
+      h4(&)
+    end
+
+    def AlertDescription(&)
+      p(&)
+    end
+
+    # Route helper stubs - return "#" as placeholder
+    def docs_sheet_path
+      "#"
+    end
+
+    def docs_separator_path
+      "#"
+    end
+
+    def docs_accordion_path
+      "#"
+    end
+
+    def docs_alert_path
+      "#"
+    end
+
+    def docs_alert_dialog_path
+      "#"
+    end
+
+    def docs_aspect_ratio_path
+      "#"
+    end
+
+    def docs_avatar_path
+      "#"
+    end
+
+    def docs_badge_path
+      "#"
+    end
   end
 end
