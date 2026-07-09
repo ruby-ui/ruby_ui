da0df2930d9fd1ee5c90b6044fecaecca8acb46f
Jean Pierry <pierrybm@gmail.com>
2026-07-09 17:34:22 -0300

    fix(native_select): corrige capitalização de viewBox no chevron do NativeSelectIcon (#467)

--- gem/lib/ruby_ui/native_select/native_select_icon.rb
+++ gem/lib/ruby_ui/native_select/native_select_icon.rb
@@ -17,7 +17,7 @@ module RubyUI
     def icon
       svg(
         xmlns: "http://www.w3.org/2000/svg",
-        viewbox: "0 0 24 24",
+        viewBox: "0 0 24 24",
         fill: "none",
         stroke: "currentColor",
         stroke_width: "2",
